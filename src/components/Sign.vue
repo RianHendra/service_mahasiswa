e5045f2b2f89fec90e8295ea6cd536f9f6793725
Rian Hendra <64124938+RianHendra@users.noreply.github.com>
2025-07-01 22:01:59 +0800

    Update Sign.vue 2

--- src/components/Sign.vue
+++ src/components/Sign.vue
@@ -120,6 +120,7 @@ export default {
           if (response.data.token) {
             localStorage.setItem('token', response.data.token)
             localStorage.setItem('user', JSON.stringify(response.data.user))
+	    localStorage.setItem('loggedIn', 'true')
             this.$router.push('/dashboard')
           } else {
             this.errorMsg = 'Login gagal.'
