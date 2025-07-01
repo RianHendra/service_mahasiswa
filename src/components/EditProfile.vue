96c80b6fee53997db30cd31b7738ff7fa5c9737d
RianHendra <rianhendra.id@gmail.com>
2025-07-02 01:54:34 +0800

    sdsdsds

--- src/components/EditProfile.vue
+++ src/components/EditProfile.vue
@@ -152,17 +152,17 @@
 										<!--begin:Menu item-->
 										<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
 											<!--begin:Menu link-->
-											<span class="menu-link">
-												<span class="menu-icon">
-													<i class="ki-duotone ki-element-11 fs-2">
-														<span class="path1"></span>
-														<span class="path2"></span>
-														<span class="path3"></span>
-														<span class="path4"></span>
-													</i>
-												</span>
-												<span class="menu-title">Dashboard</span>
-											</span>
+											<router-link to="/dashboard" class="menu-link">
+                                            <span class="menu-icon">
+                                                <i class="ki-duotone ki-element-11 fs-2">
+                                                <span class="path1"></span>
+                                                <span class="path2"></span>
+                                                <span class="path3"></span>
+                                                <span class="path4"></span>
+                                                </i>
+                                            </span>
+                                            <span class="menu-title">Dashboard</span>
+                                            </router-link>
 											<!--end:Menu link-->
 										</div>
 										<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
