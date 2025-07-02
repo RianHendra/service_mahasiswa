635b43aa4f4820a4c0a9e186d3ab6b2ce6d10914
RianHendra <rianhendra.id@gmail.com>
2025-07-02 09:49:59 +0800

    sdsds

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -481,6 +481,7 @@ export default {
       emailMhs: '',
       fotoMhs: '',
 	  orangTua: null,
+	  loading: true
     }
   },
   mounted() {
@@ -498,17 +499,20 @@ export default {
   },
   methods: {
 	 async getDataOrangtua() {
-      try {
-        const nim = localStorage.getItem('UserNim')
-        const res = await axios.get(`https://ti054d03.agussbn.my.id/api/mahasiswa/${nim}`)
-        this.orangTua = Array.isArray(res.data.ortu) && res.data.ortu.length > 0
-          ? res.data.ortu[0]
-          : null
-      } catch (error) {
-        console.error('Gagal mengambil data orang tua:', error)
-      }
-    }
-  },
+  try {
+    const nim = localStorage.getItem('UserNim')
+    const res = await axios.get(`https://ti054d03.agussbn.my.id/api/mahasiswa/orangtua`)
+
+    const filtered = res.data.ortu.filter(ortu => ortu.nim === nim)
+
+    this.orangtua = filtered
+    this.orangTua = filtered.length > 0 ? filtered[0] : null
+    this.loading = false
+  } catch (error) {
+    console.error('Gagal mengambil data orang tua:', error)
+    this.loading = false
+  }
+}
 	async deleteOrangtua(id) {
       const konfirmasi = confirm('Yakin ingin menghapus data orang tua ini?')
       if (!konfirmasi) return
